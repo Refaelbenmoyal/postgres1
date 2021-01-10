3dbb1978b4a3061b365864609c1ee2f01a636491
refaelbenmoyal <67203489+Refaelbenmoyal@users.noreply.github.com>
2021-01-10 21:59:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progres1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myconfig = ConfigurationManager.AppSettings[" postgres "];
+            private static void PrintAllworkers(string conn_string)
+            {
+                
+                using (var conn = new NpgsqlConnection(conn_string))
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM workers";
+
+                    NpgsqlCommand command = new NpgsqlCommand(query, conn);
+                    command.CommandType = System.Data.CommandType.Text;
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        long id = (long)reader["id"];
+                        string name = (string)reader["name"];
+                        
+                        Console.WriteLine($"{id} {name}");
+                        
+                    }
+                }
+            }
+
+        }
+    }
+}
